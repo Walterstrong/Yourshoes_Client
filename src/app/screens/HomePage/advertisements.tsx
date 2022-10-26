15da6fe0d9bd6a116821a076a5249644bf7440c7
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2022-10-26 23:47:24 +0900

    feat: develop homepage statistics components

---
+++ src/app/screens/HomePage/advertisements.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export function Advertisements() {
+  return <div className="top_restaurant_frame">Advertisements</div>;
+}
--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { Container } from "@mui/material";
+import { Statistics } from "./statistics";
+import { BestDishes } from "./bestDishes";
+import { Advertisements } from "./advertisements";
+import { Recommendations } from "./recommendations";
+import { Events } from "./events";
+import { TopRestaurants } from "./topRestaurants";
+import { BestRestaurants } from "./bestRestaurants";
+import "../../../css/home.css";
 
 export function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <div className="homepage">
+      <Statistics />
+      <TopRestaurants />
+      <BestRestaurants />
+      <BestDishes />
+      <Advertisements />
+      <Events />
+      <Recommendations />
+    </div>
+  );
 }
