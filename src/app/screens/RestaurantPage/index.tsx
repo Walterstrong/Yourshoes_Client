b4a557301f1aebdec7335d000f17804c8c5095f3
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2022-10-29 00:05:40 +0900

    feat: develop nester routing for restaurant screen

--- src/app/screens/RestaurantPage/index.tsx
+++ src/app/screens/RestaurantPage/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import { Container } from "@mui/material";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { ChosenDish } from "./chosenDish";
+import { OneRestaurant } from "./oneRestaurant";
+import { AllRestaurants } from "./allRestaurants";
 
 export function RestaurantPage() {
-  return <Container>RestaurantPage</Container>;
+  let restaurant = useRouteMatch();
+
+  return (
+    <div className="event_frame">
+      <Switch>
+        <Route path={`${restaurant.path}/dish/:dish_id`}>
+          <ChosenDish />
+        </Route>
+        <Route path={`${restaurant.path}/:restaurant_id`}>
+          <OneRestaurant />
+        </Route>
+        <Route path={`${restaurant.path}`}>
+          <AllRestaurants />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
---
+++ src/app/screens/RestaurantPage/oneRestaurant.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "@mui/material";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+
+export function OneRestaurant() {
+  return <div>OneRestaurant</div>;
+}
