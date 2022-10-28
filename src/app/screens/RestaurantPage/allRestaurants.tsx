a82609823993f31e9f499ace20cb0675a238d221
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2022-10-29 03:00:33 +0900

    feat: restaurant screen page allrestaurant component

--- src/app/screens/RestaurantPage/allRestaurants.tsx
+++ src/app/screens/RestaurantPage/allRestaurants.tsx
@@ -4,8 +4,8 @@ import { Container, Stack, Box, Button } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import Pagination from "@mui/material/Pagination";
 import PaginationItem from "@mui/material/PaginationItem";
-import ArrowBackIcon from "@mui/icons-material/ArrowBackIcon";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForwardIcon";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
 import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
 import AspectRatio from "@mui/joy/AspectRatio";
 import { Link } from "@mui/joy";
@@ -26,147 +26,156 @@ const order_list = Array.from(Array(8).keys());
 export function AllRestaurants() {
   return (
     <div className="all_restaurant">
-      <Stack flexDirection={"column"} alignItems={"center"}>
-        <Box className="fil_search_box">
-          <Box className="fil_box">
-            <a>Zo'r</a>
-            <a>Mashhur</a>
-            <a>Trenddagi</a>
-            <a>Yangi</a>
-          </Box>
-          <Box className="search_big_box">
-            <form className="search_form" action="" method="">
-              <input
-                type="search"
-                className="searchInput"
-                name="resSearch"
-                placeholder="Qidiriuv"
-              />
-              <Button
-                className="button_search"
-                variant="contained"
-                endIcon={<SearchIcon />}
-              >
-                Izlash
-              </Button>
-            </form>
-          </Box>
-        </Box>
-        <Stack className="all_res_box">
-          <CssVarsProvider>
-            {order_list.map((ele) => {
-              return (
-                <Card
-                  variant="outlined"
-                  sx={{ minHeight: 410, minWidth: 290, mx: "17px", my: "20px" }}
+      <Container>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Box className="fil_search_box">
+            <Box className="fil_box">
+              <a>Zo'r</a>
+              <a>Mashhur</a>
+              <a>Trenddagi</a>
+              <a>Yangi</a>
+            </Box>
+            <Box className="search_big_box">
+              <form className="search_form" action="" method="">
+                <input
+                  type="search"
+                  className="searchInput"
+                  name="resSearch"
+                  placeholder="Qidiriuv"
+                />
+                <Button
+                  className="button_search"
+                  variant="contained"
+                  endIcon={<SearchIcon />}
                 >
-                  <CardOverflow>
-                    <AspectRatio ratio="1">
-                      <img src="/restaurant/top5.png" />
-                    </AspectRatio>
-                    <IconButton
-                      aria-label="Like minimal photograpy"
-                      size="md"
-                      variant="solid"
-                      color="neutral"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                      }}
-                      sx={{
-                        position: "absolute",
-                        zIndex: 2,
-                        borderRadius: "50%",
-                        right: "1rem",
-                        bottom: 0,
-
-                        transform: "translateY(50%)",
-                        color: "rgba(0, 0, 0,.4)",
-                      }}
-                    >
-                      <Favorite style={{ color: "white" }} />
-                    </IconButton>
-                  </CardOverflow>
-                  <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
-                    Texas De Brazil
-                  </Typography>
-                  <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                    <Link href="" startDecorator={<LocationOnRoundedIcon />}>
-                      Tashkent, Yunus Abad 4-1
-                    </Link>
-                  </Typography>
-                  <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                    <Link
-                      href=""
-                      startDecorator={<CallIcon />}
-                      textColor="neutral.700"
-                    >
-                      998999999777
-                    </Link>
-                  </Typography>
-                  <CardOverflow
-                    variant="soft"
+                  Izlash
+                </Button>
+              </form>
+            </Box>
+          </Box>
+          <Stack className="all_res_box">
+            <CssVarsProvider>
+              {order_list.map((ele) => {
+                return (
+                  <Card
+                    variant="outlined"
                     sx={{
-                      display: "flex",
-                      gap: 1.5,
-                      py: 1.5,
-                      px: "var(--Card-padding",
-                      borderTop: "1px solid",
-                      borderColor: "neutral.outlinedBorder",
-                      bgcolor: "background.level1",
+                      minHeight: 410,
+                      minWidth: 290,
+                      mx: "17px",
+                      my: "20px",
                     }}
                   >
-                    <Typography
-                      level="body3"
+                    <CardOverflow>
+                      <AspectRatio ratio="1">
+                        <img src="/restaurant/top5.png" />
+                      </AspectRatio>
+                      <IconButton
+                        aria-label="Like minimal photograpy"
+                        size="md"
+                        variant="solid"
+                        color="neutral"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                        }}
+                        sx={{
+                          position: "absolute",
+                          zIndex: 2,
+                          borderRadius: "50%",
+                          right: "1rem",
+                          bottom: 0,
+
+                          transform: "translateY(50%)",
+                          color: "rgba(0, 0, 0,.4)",
+                        }}
+                      >
+                        <Favorite style={{ color: "white" }} />
+                      </IconButton>
+                    </CardOverflow>
+                    <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
+                      Texas De Brazil
+                    </Typography>
+                    <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                      <Link href="" startDecorator={<LocationOnRoundedIcon />}>
+                        Tashkent, Yunus Abad 4-1
+                      </Link>
+                    </Typography>
+                    <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                      <Link
+                        href=""
+                        startDecorator={<CallIcon />}
+                        textColor="neutral.700"
+                      >
+                        998999999777
+                      </Link>
+                    </Typography>
+                    <CardOverflow
+                      variant="soft"
                       sx={{
-                        fontWeight: "md",
-                        color: "text.secondary",
-                        alignItems: "center",
                         display: "flex",
+                        gap: 1.5,
+                        py: 1.5,
+                        px: "var(--Card-padding",
+                        borderTop: "1px solid",
+                        borderColor: "neutral.outlinedBorder",
+                        bgcolor: "background.level1",
                       }}
                     >
-                      1000{""}{" "}
-                      <VisibilityIcon
-                        sx={{ fontSize: 20, marginLeft: "5px" }}
-                      />
-                    </Typography>
-                    <Box sx={{ width: 2, bgcolor: "divider" }} />
-                    <Typography
-                      level="body3"
-                      sx={{
-                        fontWeight: "md",
-                        color: "text.secondary",
-                        alignItems: "center",
-                        display: "flex",
-                      }}
-                    >
-                      <div>500</div>
-                      <FavoriteIcon sx={{ fontSize: 20, marginLeft: "5px" }} />
-                    </Typography>
-                  </CardOverflow>
-                </Card>
-              );
-            })}
-          </CssVarsProvider>
+                      <Typography
+                        level="body3"
+                        sx={{
+                          fontWeight: "md",
+                          color: "text.secondary",
+                          alignItems: "center",
+                          display: "flex",
+                        }}
+                      >
+                        1000{""}{" "}
+                        <VisibilityIcon
+                          sx={{ fontSize: 20, marginLeft: "5px" }}
+                        />
+                      </Typography>
+                      <Box sx={{ width: 2, bgcolor: "divider" }} />
+                      <Typography
+                        level="body3"
+                        sx={{
+                          fontWeight: "md",
+                          color: "text.secondary",
+                          alignItems: "center",
+                          display: "flex",
+                        }}
+                      >
+                        <div>500</div>
+                        <FavoriteIcon
+                          sx={{ fontSize: 20, marginLeft: "5px" }}
+                        />
+                      </Typography>
+                    </CardOverflow>
+                  </Card>
+                );
+              })}
+            </CssVarsProvider>
+          </Stack>
+          <Stack className="bottom_box">
+            <img className="line_img" src="/restaurant/line.svg" />
+            <Pagination
+              count={3}
+              page={1}
+              renderItem={(item) => (
+                <PaginationItem
+                  components={{
+                    previous: ArrowBackRoundedIcon,
+                    next: ArrowForwardRoundedIcon,
+                  }}
+                  {...item}
+                  color={"secondary"}
+                />
+              )}
+            />
+            <img className="line_img_two" src="/restaurant/line_two.svg" />
+          </Stack>
         </Stack>
-        <Stack className="bottom_box">
-          <img className="line_img" scr="/restaurant/line.svg" />
-          <Pagination
-            count={3}
-            page={1}
-            renderItem={(item) => (
-              <PaginationItem
-                components={{
-                  previous: ArrowBackIcon,
-                  next: ArrowForwardIcon,
-                }}
-                {...item}
-                color={"secondary"}
-              />
-            )}
-          />
-          <img className="line_img_two" src="/restaurant/line_two.svg" />
-        </Stack>
-      </Stack>
+      </Container>
     </div>
   );
 }
