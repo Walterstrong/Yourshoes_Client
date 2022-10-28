66376ecdd8d61b5129ecc62806b8ec7a8ae8cafc
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2022-10-29 02:39:00 +0900

    feat: publishing allRestaurant sections

--- src/app/screens/RestaurantPage/index.tsx
+++ src/app/screens/RestaurantPage/index.tsx
@@ -4,12 +4,12 @@ import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { ChosenDish } from "./chosenDish";
 import { OneRestaurant } from "./oneRestaurant";
 import { AllRestaurants } from "./allRestaurants";
-
+import "../../../css/restaurant.css";
 export function RestaurantPage() {
   let restaurant = useRouteMatch();
 
   return (
-    <div className="event_frame">
+    <div className="restaurant_page">
       <Switch>
         <Route path={`${restaurant.path}/dish/:dish_id`}>
           <ChosenDish />
